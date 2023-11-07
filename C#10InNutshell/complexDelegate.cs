50e31610e7bf9a643eeb5e3b3c974f71cce19b0b
kekekuli <kekekuliobj@gmail.com>
2023-11-07 10:20:19 +0800

    完成delegate

--- C#10InNutshell/complexDelegate.cs
+++ C#10InNutshell/complexDelegate.cs
@@ -1,4 +1,5 @@
 using System;
+using System.ComponentModel;
 namespace multiDelegate
 {
     //------------Dividing line----------------
@@ -45,4 +46,89 @@ namespace multiDelegate
         static int Square(int x) => x * x;
     }
     //------------Dividing line----------------
+    public class FuncAndAction
+    {
+        // Func and Action is delegate that already implemented in .NET
+        // no difference between other custom delegate
+        delegate TResult Func<out TResult>();
+        delegate TResult Func<in T, out TResult>(T arg);
+        delegate TResult Func<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
+        //... so on
+
+        delegate void Action();
+        delegate void Action<in T>(T arg);
+        delegate void Action<in T1, in T2>(T1 arg1, T2 arg2);
+        //... so on
+    }
+    //------------Dividing line----------------
+    public class VersusInterfaces
+    {
+        // Interface can solve which delegate solved. If one of follow conditions true, choice delegate better.
+        // 1. The interface defines only a single method.
+        // 2. Multicast capabiliy is needed.
+        // 3. The subscriber needs to implement the interface multiple times.
+    }
+
+    //------------Dividing line----------------
+    public class Compatibility
+    {
+        // Delegate types are all incompatible with each other, even if they have the same signature.
+        public void CompatibilityMethod()
+        {
+            {
+                D1 d1 = Method1;
+                D2 d2 = Method1;
+                // d1 = d2; // Error
+                // but this is special
+                D2 d3 = new D2(d1); // valid
+            }
+            {
+                // Multicast delegates are considered equal if they referenct the same methods in the same order.
+                D d1 = Method1;
+                D d2 = Method1;
+                Console.WriteLine(d1 == d2); // True
+            }
+        }
+        delegate void D1();
+        delegate void D2();
+        delegate void D();
+        void Method1() { }
+    }
+    //------------Dividing line----------------
+    public class ParameterCompatibility
+    {
+        // A delegate can have more specific parameter types than the delegate it is assigned to.
+        // Method call this thing "ordinary polymorphic behavior", while delegate call this thing "contravariance".
+        public void ParameterCompatibilityMethod()
+        {
+            StringAction sa = new StringAction(ActOnObject);
+            sa("Hello"); 
+        }
+        void ActOnObject(object o) => Console.WriteLine(o);
+        delegate void StringAction(String s);
+    }
+    //------------Dividing line----------------
+    public class ReturnCompatibility
+    {
+        // A delegate can have a more specific return type than the delegate it is assigned to.
+        // Method call this thing "ordinary polymorphic behavior", while delegate call this thing "covariance".
+        public void ReturnCompatibilityMethod()
+        {
+            ObjectRetriever or = new ObjectRetriever(GetString);
+            object o = or();
+            Console.WriteLine(o);
+        }
+        string GetString() => "Hello";
+        delegate object ObjectRetriever();
+    }
+    //------------Dividing line----------------
+    // Generic delegate type parameter variance
+    // ...
+    /*
+        Func<string> x = ...;
+        Func<object> y = x; // valid
+
+        Action<object> x = ...;
+        Action<string> y = x; // valid
+    */
 }
